26b7a7dfac3da0d0d871bba0c2d64d562cf93d9e
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-01-14 00:00:31 +0330

    2023.01.13 - 2 (everything except $social and $projects)

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
 <?php
+// Load the template
+$tmp = file_get_contents("welcome/temp.html");
+
+// Process the current page: Main
+$main = file_get_contents("welcome/main.html");
+$social = '
+    <span tal:repeat="s social" class="social"
+          data-bs-toggle="tooltip" data-bs-placement="top" title="${s.title}"
+          itemprop="mainEntityOfPage" itemscope itemtype="https://schema.org/ProfilePage">
+      <a href="${s.link}" target="_blank" itemprop="sameAs">
+        <img src="%ROOT%/img/${s.icon}.svg" itemprop="thumbnailUrl">
+      </a>
+    </span>';
+$projects = '
+    <section class="grid-item ${p.id}" itemprop="subjectOf"
+             itemscope itemtype="https://schema.org/${p.microType}"
+             tal:repeat="p projects">
+      <div class="grid-inner animated-button">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <img src="%ROOT%/img/${p.icon}_round.png" class="icon" itemprop="logo">
+        <p class="projName" itemprop="name" tal:content="p.name"/>
+        <p class="projDesc" itemprop="disambiguatingDescription" tal:content="p.desc"/>
+        <div tal:repeat="a p.anchors" class="anchor">
+          <hr>
+          <a href="${a.link}" target="_blank" itemprop="${a.microType}"
+             data-bs-toggle="tooltip" data-bs-placement="top" title="${a.title}">${a.name}</a>
+        </div>
+      </div>
+      <meta itemprop="operatingSystem" content="${p.os}">
+      <meta itemprop="isAccessibleForFree" content="true">
+      <meta itemprop="applicationCategory" content="${p.category}">
+    </section>';
+
+// Merge it with the template
+$tmp = str_replace("%CONTENT%", $main, $tmp);
+$data = array(
+    "ROOT" => "/welcome",
+    "PAGE" => "main",
+    "TITLE" => "Mahdi Parastesh",
+    "FAVICON" => "", //"https://mahdi.mahdiparastesh.ir/Images/fav-icon.ico",
+    "HELP" => $_GET["hl"] ?? "",
+    "COUNTRY" => "",
+);
+$keys = array();
+foreach (array_keys($data) as $k) $keys[] = "%$k%";
+$tmp = str_replace($keys, array_values($data), $tmp);
+echo $tmp;
---
+++ welcome/main.js
@@ -0,0 +1,93 @@
+window.onload = function() {
+	// Initialize the Grid view
+	$('.grid').masonry({
+        itemSelector: '.grid-item'
+    });
+	
+	$("#loading").fadeOut(315);
+	$("body").css("overflow-y", "scroll");
+	$("main").animate({opacity: 1}, 636);
+};
+
+// Initialize the Popups everywhere
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+// Translation
+function translate(hl) {
+    let dict;
+    switch (hl) {
+        case "fa":
+            document.title = "مهدی پرستش";
+            $("figcaption").text("مهدی پرستش");
+            $("blockquote").html("بنده مدت ۷ سال در برنامه نویسی فولستک (بک اند + فرانت اند) و ۴ سال در برنامه نویسی اندروید (با زبان ها  «جاوا» و «کاتلین»، بعلاوه فریمورک «فلاتر») تجربه دارم. از ۱۳ سالگی (۱۳۹۲) زبان های جاواسکریپت و PHP را یاد گرفتم. از سال ۱۳۹۷ به برنامه نویسی اندروید مشغول شدم. از سال ۱۳۹۹ نیز به یادگیری سیستم عامل های لینوکسی (خصوصا فدورا، اوبونتو و سنتوس) و یونیکسی (FreeBSD و OpenBSD)، بعلاوه Windows Server و کسب تجربه در آنها مشغول شدم. از سال ۱۴۰۰، الگوریتم های «هوش مصنوعی» را یاد گرفته و تا حدود یک سال مشغول مطالعه و تمرین در این زمینه بودم.");
+            $("#cv").text("مشاهده رزومه من");
+            $(".mergen4 .projName").text("مِرگِن ۴");
+            $(".mergen4 .projDesc").text("یک ربات نرم افزاری هوش مصنوعی منطقی دارای حس های چندگانه که موقتا برای اندروید طراحی می شود.");
+			$(".instatools .projName").text("اینستاتولز");
+            $(".instatools .projDesc").text("آنفالویاب، دانلود هر گونه پست و استوری از اینستاگرام، دانلود پست های سیو شده و استخراج دایرکت ها در HTML ،PDF  و TXT.");
+            $(".telexporter .projName").text("تلکسپورتر");
+            $(".telexporter .projDesc").text("اس ام اس ها و تاریخچه تماس های خود را در قالب های وب، پی دی اف و تکست استخراج کنید.");
+            $(".migratio .projName").text("میگراتیو");
+            $(".migratio .projDesc").text("یک ابزار جغرافی-آماری برای تعیین کردن بهترین مقصد مهاجرت افراد مختلف.");
+			$(".mergen3 .projName").text("مِرگِن ۳");
+            $(".mergen3 .projDesc").text("یک نرم افزار رباتیک هوش مصنوعی برای پردازش چند نوع حس های متفاوت که به یک سرور و یک یا چند کلاینت نیاز داشت. این روش بعدا با روشی جایگزین شد که به سرور و کلاینت نیاز نداشت. (پروژه آرشیو شده)");
+			$(".mergen2 .projName").text("مِرگِن ۲");
+            $(".mergen2 .projDesc").text("یک ربات نرم افزاری صوتی (شنیداری و گفتاری) برای پردازش زبان انسان، که بعدا با یک نرم افزار برای پردازش هم صوت و هم تصویر جایگزین شد. (پروژه آرشیو شده)");
+			$(".mergen1 .projName").text("مِرگِن ۱");
+            $(".mergen1 .projDesc").text("یک نرم افزار هوش مصنوعی منطقی برای پردازش زبان انسانی؛ که قرار بود فقط از راه متن دیجیتال تفکر کند و بعدا با یک نرم افزاری پردازش صوت جایگزین شد. (پروژه آرشیو شده)");
+            $(".fortuna .projName").text("فورتونا");
+            $(".fortuna .projDesc").text("نرم افزاری بر مبنای فلسفه «هدونیسم»");
+            $(".friend_tracker .projName").text("ردیاب دوستان");
+            $(".friend_tracker .projDesc").text("دوستان خود را از روی نقشه ردیابی کنید. (پروژه آرشیو شده)");
+            $(".saam .projName").text("سام");
+            $(".saam .projDesc").text("گرداور و ذخیره کننده اطلاعات بورس، ساخته شده بر مبنای متاتریدر ۵ (پروژه آرشیو شده)");
+            $(".sexbook .projName").text("سکسبوک");
+            $(".sexbook .projDesc").text("زندگی جنسی خود را ثبت کرده و آمارگیری و کنترل کنید.");
+			$("body").css("font-family", "IRANYekan");
+			dict = {
+			    "Google Play": "گوگل پلی",
+			    "Galaxy Store": "گلکسی استور",
+			    "Website": "وبسایت",
+			    "Website (Demo)": "وبسایت (دمو)",
+			    "Android Source": "سورس اندروید",
+			    "Android Source (Kotlin)": "سورس اندروید (کاتلین)",
+			    "Android Source (Java)": "سورس اندروید (جاوا)",
+			    "Flutter Source": "سورس فلاتر",
+			    "Software Source (Python)": "سورس نرم افزار (پایتون)",
+			    "Web Template": "قالب وب",
+			    "Server Source (Python)": "سورس سرور (پایتون)",
+			    "NashStore": "ناشستور",
+			    "Myket": "مایکت",
+			    "Bazaar": "بازار",
+			    "Privacy Policy": "سیاست حریم خصوصی",
+				"Russian App Store": "فروشگاه اپلیکیشن روسی ناشستور",
+				"Iranian Android Myket Store": "فروشگاه اپلیکیشن ایرانی مایکت",
+				"Iranian Android Bazaar Store": "فروشگاه اپلیکیشن ایرانی بازار",
+				"APKPure.com a third-party APK store": "یک وبسایت معروف دانلود اپ اندروید",
+				"Download for Android (Gregorian calendar)": "دانلود برای اندروید (تقویم میلادی)",
+				"Download for Android (Persian calendar)": "دانلود برای اندروید (تقویم شمسی)",
+				"Wordpress Theme": "تم وردپرس",
+			};
+            break;
+		default:
+		    document.location.assign(document.location + ((document.location.toString().indexOf("?") == -1) ? "?" : "&") + "hl=en");
+			break;
+    }
+    if (dict) $(".anchor a").each(function() {
+		$(this).text(dict[$(this).text()]);
+		$(this).attr("data-bs-original-title", dict[$(this).attr("data-bs-original-title")]);
+	});
+	if (["fa", "ar"].includes(hl))
+	    $("blockquote, .projDesc").css("direction", "rtl");
+	$('.grid').masonry();
+	$("#langSelect").attr("src",$("#lang li img[data-lang="+hl+"]").attr("src"));
+}
+$("#lang li img").on('click', function(e) { translate($(this).attr("data-lang")); });
+if ($("#help").val() != "en") switch ($("#country").val()) {
+    case "IR":
+        translate("fa");
+        break;
+}
